00613151113d6ff9e8f4fda923ed976ab0038934
gavrakr <gavrakrt@naver.com>
2022-09-27 11:03:52 +0900

    20220927-1

--- core/models.py
+++ core/models.py
@@ -4,8 +4,8 @@ class TimeStampModel(models.Model):
 
     """Time Stamp Model"""
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
